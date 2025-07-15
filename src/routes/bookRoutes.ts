1a6171e93b692fefb3bc1dc43d3568b5ecb45a75
blckfrost <beataanima88@gmail.com>
2025-07-15 11:55:27 +0000

    feat(book): book routes create, update

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -16,10 +16,12 @@ router.get('/', async (req, res) => {
             return res.status(404).json({ message: 'Books not found' });
         }
         res.json(books);
-    } catch (err) {}
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching book' });
+    }
 });
 
-// GET all Books
+// GET one Books
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
 
@@ -39,13 +41,66 @@ router.get('/:id', async (req, res) => {
         }
         res.json(book);
     } catch (err) {
-        res.status(500).json({ message: 'Error fetching book' });
+        res.status(500).json({ error: 'Error fetching book' });
     }
 });
 
 // Create Book
+router.post('/', async (req, res) => {
+    const { title, author, type } = req.body;
+
+    if (!title || !author || !type) {
+        return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    try {
+        const createBook = await prisma.book.create({
+            data: {
+                userId: req.userId,
+                title: title,
+                author: author,
+                type: type,
+            },
+        });
+        res.status(201).json(createBook);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error creating book' });
+    }
+});
 
 // Update Book
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, author, type, isRead, totalPage, currentPage } = req.body;
+    try {
+        const book = await prisma.book.findUnique({
+            where: { id },
+        });
+        if (!book) {
+            return res.status(404).json({ error: 'Book not found' });
+        }
+
+        if (!book || book.userId !== req.userId) {
+            return res.status(403).json({ error: 'Not authorized' });
+        }
+        const updateBook = await prisma.book.update({
+            where: { id },
+            data: {
+                ...(title !== undefined && { title }),
+                ...(author !== undefined && { author }),
+                ...(type !== undefined && { type }),
+                ...(isRead !== undefined && { isRead }),
+                ...(totalPage !== undefined && { totalPage }),
+                ...(currentPage !== undefined && { currentPage }),
+            },
+        });
+        res.json(updateBook);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Failed to update book' });
+    }
+});
 
 // Delete Book
 
